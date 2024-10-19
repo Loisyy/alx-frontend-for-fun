052872340b5065e0fff5715508d09136964a56c5
Nkeiru Lois <nkeiruloisy@yahoo.com>
2024-10-19 00:53:51 +0000

    task 2

--- markdown2html.py
+++ markdown2html.py
@@ -19,6 +19,8 @@ if __name__ == '__main__':
 
     # Open the Markdown file and the output HTML file
     with open(sys.argv[1], 'r') as md_file, open(sys.argv[2], 'w') as html_file:
+        unordered_start = False  # Keep track of unordered list state
+        
         for line in md_file:
             # Remove any leading/trailing whitespace
             line = line.strip()
@@ -28,12 +30,30 @@ if __name__ == '__main__':
             headings = line.lstrip('#')
             heading_num = length - len(headings)
 
+            unordered = line.lstrip('-')
+            unordered_num = length - len(unordered)
+
             # If the line starts with 1-6 '#' characters, it's a heading
             if 1 <= heading_num <= 6:
                 line = '<h{}>'.format(heading_num) + headings.strip() + '</h{}>\n'.format(heading_num)
 
+            # Parse unordered list items
+            if unordered_num:
+                if not unordered_start:
+                    html_file.write('<ul>\n')
+                    unordered_start = True
+                line = '<li>' + unordered.strip() + '</li>\n'
+
+            if unordered_start and not unordered_num:
+                html_file.write('</ul>\n')
+                unordered_start = False
+
             # Write the converted line to the HTML file
             html_file.write(line)
+
+        # Close any open unordered list at the end of the file
+        if unordered_start:
+            html_file.write('</ul>\n')
 
     # If no issues, exit successfully
     exit(0)
